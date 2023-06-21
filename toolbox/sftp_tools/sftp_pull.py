fd5379beb5af5f7300bc83c68b2e7b215f95dd96
dbellieny <daniel.bellieny@gmail.com>
2023-06-21 15:26:49 -0700

    SFTP background download tool

---
+++ toolbox/sftp_tools/sftp_pull.py
@@ -0,0 +1,25 @@
+import pysftp
+
+
+def transfer_callback(event, chunk):
+    # Callback function to track the transfer progress
+    if event == 'put':
+        # File upload event
+        print(f"Uploaded {chunk} bytes")
+    elif event == 'get':
+        # File download event
+        print(f"Downloaded {chunk} bytes")
+
+
+# Create an instance of the `pysftp.Connection` class
+with pysftp.Connection('sftp.genewiz.com',
+                       username='isylvain_berkeley',
+                       password='aS06jJvl2oMG0MvKIBtx') as sftp:
+
+    # Change to the desired remote directory
+    sftp.chdir('30-857011403/00_fastq/')
+
+    # Start the background file transfer
+    sftp.put('/groups/doudna/team_resources/azenta_temp/30-857011403', callback=transfer_callback)
+
+# The file transfer is running in the background after the `with` block is exited

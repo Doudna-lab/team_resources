1fdd650ba9f102c0f3d770bdf37d7e14976197b3
dbellieny <daniel.bellieny@gmail.com>
2023-06-25 11:43:11 -0700

    md5 hash file check

---
+++ toolbox/baseSpace_tools/md5_check.py
@@ -0,0 +1,34 @@
+import os
+import hashlib
+
+
+def compare_hashes_in_directory(directory):
+	for filename in os.listdir(directory):
+		file_path = os.path.join(directory, filename)
+		if os.path.isfile(file_path):
+			md5_hash = hashlib.md5()
+			with open(file_path, "rb") as file:
+				# Read the file in chunks to avoid memory issues with large files
+				for chunk in iter(lambda: file.read(4096), b""):
+					md5_hash.update(chunk)
+
+			md5_hash_value = md5_hash.hexdigest()
+			expected_hash_file_path = file_path.rsplit(".", 1)[0] + ".txt"  # Assume hash value is stored in a .txt file
+
+			with open(expected_hash_file_path, "r") as hash_file:
+				expected_hash = hash_file.readline().strip()
+
+			if md5_hash_value == expected_hash:
+				print(f"Hash match for file: {filename}")
+			else:
+				print(f"Hash mismatch for file: {filename}")
+
+
+def main():
+	# Provide the directory path to compare hashes
+	directory_path = "path/to/directory"
+	compare_hashes_in_directory(directory_path)
+
+
+if __name__ == "__main__":
+	main()

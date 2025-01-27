cfb387d97b231b9e232b207be2a2959627a7c2bc
dbellieny <daniel.bellieny@gmail.com>
2025-01-27 14:44:51 -0800

    hisat3n_tools: mpiexec on sbatch with example

--- toolbox/hisat3n_tools/sh/sbatch_maxconvs.sh
+++ toolbox/hisat3n_tools/sh/sbatch_maxconvs.sh
@@ -7,4 +7,5 @@ eval "$(conda shell.bash hook)"
 conda activate biopympi
 cd '/groups/doudna/team_resources/toolbox/hisat3n_tools/'
 
-mpiexec python py/process_maxconv.py /groups/doudna/projects/daniel_projects/hisat-3n_test_files/124-M_120_mapping.sorted.bam /groups/doudna/projects/daniel_projects/hisat-3n_test_files/ED_o_041.fasta 'G' 'A' 1 delete_test.bam report.log
+mpiexec python py/process_maxconv.py <input_BAM> <reference_fasta> <original_base> <converted_base> <maximum_conversions> <output_BAM> <log_file>
+# mpiexec python py/process_maxconv.py /groups/doudna/projects/daniel_projects/hisat-3n_test_files/124-M_120_mapping.sorted.bam /groups/doudna/projects/daniel_projects/hisat-3n_test_files/ED_o_041.fasta 'G' 'A' 1 delete_test.bam report.log
